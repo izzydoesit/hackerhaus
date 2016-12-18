ad1def6da07a07a5e329e555be33580479070c9b
Izzy Matos <israeldmatos@gmail.com>
2016-12-18 11:27:09 -0800

    Add new User var & create action in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new(:role => 'agent')
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to root_path, notice: 'Sign up complete. Please check your inbox for activation email.'
+    else
+      render action: "new"
+    end
   end
 end
