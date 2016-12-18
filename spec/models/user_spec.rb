ed51e8b9a5aa232ee06ccd4071d7107f62d1423e
Izzy Matos <israeldmatos@gmail.com>
2016-12-18 10:57:38 -0800

    Add tests for user type authenticaton and permissions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a name, email and password confirmation" do 
+    u = create(:user, :agent)
+    expect(u).to be_valid
+  end
+  
+  it "sends a confirmation email after sign up" do 
+    u = create(:user, :agent)
+    open_last_email.should be_delivered_to u.email
+  end
+  
+  it "is invalid without email" do 
+    u = create(:user, :agent, :email=> nil)
+    expect(u).to have(1).errors_on(:email)
+  end
+  
+  it "is invalid without a name" do 
+    u = build(:user, :agent, :email=> nil)
+    expect(u).to have(1).errors_on(:name)
+  end
+  
+  it "is invalid if passwords don't match'" do 
+    u = build(:user, :agent, :password => "pw1", :password_confirmation => "wp1")
+    expect(u).to have(1).errors_on(:password)
+  end
+  
+  it "is invalid if no activation code is generated" do 
+    u = create(:user, :agent)
+    expect(u.activation_code).to_not be_nil
+  end
+
 end
