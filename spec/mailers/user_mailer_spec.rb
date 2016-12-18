d8956c0bebe849f0a39cab29e07ed7fb083e125a
Izzy Matos <israeldmatos@gmail.com>
2016-12-18 11:09:39 -0800

    Add user mailer tests for delivery & activation code

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,19 @@
-require "rails_helper"
+require "spec_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include Rails.application.routes.url_helpers
+
+  before(:all) do 
+    @user = create(:user, :agent)
+    @email = UserMailer.confirmation_email(@user)
+  end
+
+  it 'should be sent to the user email' do 
+    @email.should deliver_to(@user.email)
+  end
+
+  it 'should contain the activation link inside' do 
+    pending('Uncomment after activation function works')
+    # @email.should have_body_text(/#{activate_path(:code => @user.activation_code)}/)
+  end
 end
