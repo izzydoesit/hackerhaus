be979a33d525312ba463ccbd1cde17b814870dfd
Izzy Matos <israeldmatos@gmail.com>
2016-12-18 09:08:57 -0800

    Add sphinx for testing search feature

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,14 +30,27 @@ RSpec.configure do |config|
   # config.mock_with :flexmock
   # config.mock_with :rr
 
+  config.before(:suite) do
+      ThinkingSphinx::Test.init
+      ThinkingSphinx::Test.start_with_autostop
+    end
+
+  config.before(:each) do
+    ThinkingSphinx::Test.index if example.metadata[:js]
+  end
+
+  config.after(:each) do
+    Property.delete_all
+  end
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
-
+  config.use_transactional_fixtures = false
+  
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
@@ -48,7 +61,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
-  
+
   config.include FactoryGirl::Syntax::Methods
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
