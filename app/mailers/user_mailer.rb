7b65617d011f4b6efb37cecdded950ce51a3a057
Izzy Matos <israeldmatos@gmail.com>
2016-12-18 11:11:51 -0800

    Add confirmation email method for user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,8 @@
 class UserMailer < ApplicationMailer
+  default from: "ericidle@example.com"
+
+  def confirmation_email(user)
+    @user = user
+    mail(:to => user.email, :subject => "Please Activate Your Account")
+  end
 end
