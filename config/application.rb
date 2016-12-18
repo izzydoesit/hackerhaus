c2c6c80fddc730ebcaad58f979b30cc5e8e090a8
Izzy Matos <israeldmatos@gmail.com>
2016-12-18 03:51:59 -0800

    Config test framework generator

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,12 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RealEstate
+module Hackerhouses
   class Application < Rails::Application
+    config.generators do |g|
+      g.test_framework :rspec, fixtures: true, view_specs: false, helper_specs: false,
+      routing_specs: false, controller_specs: true, request_specs: false
+      g.fixtures_replacement :factory_girl, dir: "spec/factories"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
