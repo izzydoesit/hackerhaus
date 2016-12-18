77da61413571632a54412de25098ca02a1c87997
Izzy Matos <israeldmatos@gmail.com>
2016-12-18 08:18:31 -0800

    Add registration for Search component in home page

---
+++ client/app/bundles/Home/startup/registration.jsx
@@ -0,0 +1,6 @@
+import ReactOnRails from 'react-on-rails';
+
+import Search from '../components/Search';
+
+// This is how react_on_rails can see the HelloWorld in the browser.
+ReactOnRails.register({ Search });
