fe1f985c7b1374e73b2b17c68d456dc4d51b3182
Izzy Matos <israeldmatos@gmail.com>
2016-12-18 10:39:35 -0800

    Add user helper module to spec helper file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/rspec'
+require 'email_spec'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -48,8 +49,11 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
-  
+
   config.include FactoryGirl::Syntax::Methods
+  config.include(UserHelper)
+  config.include(EmailSpec::Helpers)
+  config.include(EmailSpec::Matchers)
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
