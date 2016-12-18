ec003e5ac9f6cb9de53d3ce817e5c80d9addca64
Izzy Matos <israeldmatos@gmail.com>
2016-12-18 05:01:55 -0800

    Install react_on_rails gem

--- Gemfile
+++ Gemfile
@@ -70,3 +70,5 @@ end
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 HackerHouses::Application.routes.draw do
+  get 'hello_world', to: 'hello_world#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
