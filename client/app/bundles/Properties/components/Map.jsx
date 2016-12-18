12b0ab555bf4b009f7ddad00043e3b5f97853d45
Izzy Matos <israeldmatos@gmail.com>
2016-12-18 08:16:09 -0800

    Add basic map component

---
+++ client/app/bundles/Properties/components/Map.jsx
@@ -0,0 +1,16 @@
+import React, { PropTypes } from 'react';
+
+export default class Map extends React.Component {
+  constructor(props, _railsContext) {
+    super(props);
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>This is my map</h1>
+      </div>
+    )
+  }
+}
