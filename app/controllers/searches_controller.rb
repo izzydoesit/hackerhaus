29b97df39ebd6652eeee7979bbd2da70a1ad9ff3
Izzy Matos <israeldmatos@gmail.com>
2016-12-18 21:17:20 -0800

    Add search action to Searches controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,4 +2,12 @@ class SearchesController < ApplicationController
   def show
     @properties = Property.search params[:query]
   end
+
+  def search 
+    if params[:query].nil?
+      @properties = []
+    else
+      @properties = Property.search params[:query]
+    end
+  end
 end
