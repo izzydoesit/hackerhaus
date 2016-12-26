00a5b60bacc7070641a87f7174f617e546499723
Izzy Matos <israeldmatos@gmail.com>
2016-12-26 02:28:08 -0800

    Add hoods variable to home controller index action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
 
 	def index
-		@properties = Property.order("created_at DESC").limit(5)
+		# @properties = Property.order("created_at DESC").limit(5)
+		@hoods = [ 'SOMA', 'Tenderloin', 'Lower Nob', 'Richmond', 'Mission District', 'Castro']
 	end
 
 end
