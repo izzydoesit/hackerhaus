e33b3cb80b4870bc756c7c388d88dd72781345b8
Izzy Matos <israeldmatos@gmail.com>
2016-12-26 05:39:06 -0800

    Set config options to compile assets and serve them

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Hackerhaus::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  # config.serve_static_files = false
+  config.serve_static_files = true
   config.public_file_server.enabled = false
 
   # Compress JavaScripts and CSS.
@@ -28,7 +28,7 @@ Hackerhaus::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
