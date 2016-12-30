5691c5b4812e3653f874795dbd03ca0a12f2e856
Izzy Matos <israeldmatos@gmail.com>
2016-12-30 15:05:46 -0800

    Fix merge conflict

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,9 @@
 class HomeController < ApplicationController
 
 	def index
-<<<<<<< HEAD
 		@properties = Property.order("created_at DESC").limit(5)
 		# @properties = Property.order("created_at DESC").limit(5)
 		@hoods = [ ['SOMA', 'soma.jpg'], ['Tenderloin', 'tenderloin.jpg'], ['Lower Nob', 'lowernob.jpg'], ['Richmond', 'richmond.jpg'], ['Mission District', 'mission.jpg'], ['Castro', 'castro.jpg']]
-=======
-		# @properties = Property.order("created_at DESC").limit(5)
-		@hoods = [ 'SOMA', 'Tenderloin', 'Lower Nob', 'Richmond', 'Mission District', 'Castro']
->>>>>>> a716afbf87138325ceb90bc38ad59464a338c38d
 	end
 
 end
