0d8ec453ba4dce59be9150ac2d5391f04f0e05dc
Izzy Matos <israeldmatos@gmail.com>
2016-12-30 15:04:14 -0800

    Add variable of hood titles and pic refs to home page view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
 
 	def index
 		@properties = Property.order("created_at DESC").limit(5)
+		# @properties = Property.order("created_at DESC").limit(5)
+		@hoods = [ ['SOMA', 'soma.jpg'], ['Tenderloin', 'tenderloin.jpg'], ['Lower Nob', 'lowernob.jpg'], ['Richmond', 'richmond.jpg'], ['Mission District', 'mission.jpg'], ['Castro', 'castro.jpg']]
 	end
 
 end
