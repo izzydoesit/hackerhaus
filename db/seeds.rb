e6015b76df4bb4924b19507c08334ef8b529ad74
Izzy <izzy@Izzys-MacBook-Pro.local>
2017-01-30 14:37:25 -0800

    Add seed data with address info for geocoding

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,8 @@ titles = ['Bed with a view', 'Conscious community living', 'LGBTQ living in SF',
 cities = ['SOMA', 'Mission District', 'Richmond', 'The Castro', 'North Beach']
 descriptions = ['Luxury penthouse overlooking the river', 'Craftsman townhouse with beautiful detail', 'Quaint quiet house near the park, great for dogs', 'Middle of downtown, filled with nightlife and plenty of shops nearby', 'Tucked away on a side street in a busy corner of town. Best of both worlds.', 'Conscious community practicing emotional intelligence and equity sharing', 'House with a garden and a great set of folks!', 'Incredible new startup incubator a la Silicon Valley!!']
 
-50.times do
-  Property.create(title: titles.sample, description: descriptions.sample, city_name: cities.sample, price: rand(500..3000))
-end
+
+Property.create(title: 'Hacker house in SOMA', description: 'Hack your way to startup riches with us!', street: '221 7th st', city: 'San Francisco', state: 'CA', zip_code: 94103, price: rand(500..3000))
+Property.create(title: 'Conscious Vegan Community Living', description: 'Conscious community practicing emotional intelligence and equity sharing in the Castro', street: '120 Eureka St', city: 'San Francisco', state: 'CA', zip_code: 94114, price: rand(500..3000))
+Property.create(title: 'Burner neoHippie Haven', description: 'Live on the edge of the future while remaining vintage in this Mission Dolores townhouse with 10 roommmates', street: '3143 16th St', city: 'San Francisco', state: 'CA', zip_code: 94103, price: rand(500..3000))
+
