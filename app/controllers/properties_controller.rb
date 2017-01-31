3b0af268b35fdf5190691dbdd2d1fd7f4d03aaa9
Izzy <izzy@Izzys-MacBook-Pro.local>
2017-01-31 04:45:06 -0800

    Add new and create actions to properties controller

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -4,4 +4,22 @@ class PropertiesController < ApplicationController
 		@properties = Property.order("created_at DESC").limit(5)
 	end
 
+	def new
+		@property = Property.new
+	end
+
+	def create
+		@property = Property.new(property_params)
+		if @property.save
+			flash[:notice] = "Congratulations.  Your property has been listed with H@ackerhaus."
+			redirect_to search_path
+		else
+			render :new
+		end
+	end
+
+	def property_params
+		params.require(:property).permit(:file, :title, :description, :street, :city, :state, :zip_code, :price, :image)
+	end
+
 end
