b66066a7738ad8422df3522ed2acbd8eb81f151a
Izzy <izzy@Izzys-MacBook-Pro.local>
2017-01-31 01:42:05 -0800

    Add custom search and index settings to Property model

--- app/models/property.rb
+++ app/models/property.rb
@@ -12,9 +12,31 @@ class Property < ActiveRecord::Base
   validates :price, :presence => true
   geocoded_by :address
   after_validation :geocode, :if => :street_changed? || :city_changed? || :state_changed?
-
+  ac_field :title, search_fields: [:title, :city]
+  
   def address
     [street, city, state].compact.join(', ')
+  end
+
+  def self.search(query)
+    __elasticsearch__.search(
+      {
+        query: {
+          multi_match: {
+            query: query,
+            fields: ['title^10', 'description', 'street', 'city']
+          }
+        }
+      }
+    )
+  end
+
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :title, analyzer: 'english'
+      indexes :description, analyzer: 'english'
+      indexes :city, analyzer: 'english'
+    end
   end
 
 end
