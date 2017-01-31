3ebab97aa6877930a6cd75b6276cc8b75f1aa9d2
Izzy <izzy@Izzys-MacBook-Pro.local>
2017-01-30 21:01:04 -0800

    Add property_markers action to render geojson

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,9 +1,38 @@
 class SearchesController < ApplicationController
+
   def show
     if params[:query].nil?
-      @properties = []
+      @properties = Property.all
     else
-      @properties = Property.search params[:query]
+      @properties = Property.search(params[:query]).records
     end
   end
+
+  def property_markers
+    @locations = Property.all
+    @geojson = Array.new
+
+    @locations.each do |listing|
+      @geojson << {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [listing.longitude, listing.latitude]
+        },
+        properties: {
+          name: listing.title,
+          address: listing.street,
+          :'marker-color' => '#00607d',
+          :'marker-symbol' => 'circle',
+          :'marker-size' => 'medium'
+        }
+      }
+      
+      respond_to do |format|
+        format.html
+        format.json { render :json => @geojson and return }
+      end
+    end
+  end
+
 end
