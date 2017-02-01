a3b83476bb293bff0a2ff5f46b537d36ee6e8e46
Izzy <izzy@Izzys-MacBook-Pro.local>
2017-02-01 13:49:13 -0800

    Add states array var for form & redirect after submit

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -6,13 +6,14 @@ class PropertiesController < ApplicationController
 
 	def new
 		@property = Property.new
+		@states = ['AL', 'AR', 'AK', 'CA', 'CT', 'MA', 'MI', 'MO', 'NJ', 'NY', 'OR', 'WA']
 	end
 
 	def create
 		@property = Property.new(property_params)
 		if @property.save
 			flash[:notice] = "Congratulations.  Your property has been listed with H@ackerhaus."
-			redirect_to search_path
+			redirect_to root_path
 		else
 			render :new
 		end
