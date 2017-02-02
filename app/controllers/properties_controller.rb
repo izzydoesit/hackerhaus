015ff8df4e52bc777730ee61d7115f572043baf8
Izzy <izzy@Izzys-MacBook-Pro.local>
2017-02-02 10:29:00 -0800

    Add all states to the new Property form

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -6,7 +6,7 @@ class PropertiesController < ApplicationController
 
 	def new
 		@property = Property.new
-		@states = ['AL', 'AR', 'AK', 'CA', 'CT', 'MA', 'MI', 'MO', 'NJ', 'NY', 'OR', 'WA']
+		@states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA','MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI']
 	end
 
 	def create
