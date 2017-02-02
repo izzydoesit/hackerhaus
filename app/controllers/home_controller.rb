c39524d3c245fc10844d129c21eb7126e86caa39
Izzy <izzy@Izzys-MacBook-Pro.local>
2017-02-02 10:02:14 -0800

    Changed neighborhood on home page to North Beach

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 	def index
 		@properties = Property.order("created_at DESC").limit(5)
 		# @properties = Property.order("created_at DESC").limit(5)
-		@hoods = [ ['SOMA', 'soma.jpg'], ['Castro', 'castro.jpg'], ['Lower Nob', 'lowernob.jpg'], ['Richmond', 'richmond.jpg'], ['Mission District', 'mission.jpg'], ['Tenderloin', 'tenderloin.jpg']]
+		@hoods = [ ['SOMA', 'soma.jpg'], ['Castro', 'castro.jpg'], ['North Beach', 'lowernob.jpg'], ['Richmond', 'richmond.jpg'], ['Mission District', 'mission.jpg'], ['Tenderloin', 'tenderloin.jpg']]
 	end
 
 end
